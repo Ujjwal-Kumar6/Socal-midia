1f0df190d2354f82151b8a8cfc3c4624648ebcf7
Ujjwal Kumar <uk7320942276@gmail.com>
2026-02-21 16:51:59 +0530

    feat: add video call functionality

--- forntend/src/pages/Conv.jsx
+++ forntend/src/pages/Conv.jsx
@@ -12,7 +12,7 @@ import { BiLoader } from 'react-icons/bi';
 import { setMessages, clearSelectedUser } from '../redux/massageSlice';
 import SenderMassage from '../componesnsts/SenderMassage';
 import ReciverMassage from '../componesnsts/ReciverMassage';
-import VideoCall from '../componesnsts/VidioCall';
+import VideoCall from './pages/VidioCall.jsx';
 
 function Conv() {
   const { selectedUser, messages } = useSelector(state => state.message);
