4db2e5928de2a9375d3403912f7d3ea5cb528828
Ujjwal kumar <uk7320942276@gmail.com>
2026-03-12 11:53:56 +0530

    Update main.jsx

--- forntend/src/main.jsx
+++ forntend/src/main.jsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, RouterProvider } from 'react-router-dom'
-import {Provider} from "react-redux"
-import store from './redux/store.js'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
